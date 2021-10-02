799fac7f15309563a484f53d795ea7e1acb50db8
Warren772 <82717393+beyondmafiadev-warrens@users.noreply.github.com>
2021-10-02 01:32:31 -0400

    Game Page skeleton

---
+++ src/components/MafiaGame/GamePage.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class GamePage extends Component {
+  constructor(props){
+      super(props);
+	  this.state = {
+      }
+  }
+render(){
+  return(
+<div>
+    
+</div>
+);
+}
+}
+
+export default GamePage;
---
+++ src/components/MafiaGame/PlayerContainer.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class PlayerContainer extends Component {
+  constructor(props){
+      super(props);
+	  this.state = {
+      }
+  }
+render(){
+  return(
+<div>
+    
+</div>
+);
+}
+}
+
+export default PlayerContainer;
