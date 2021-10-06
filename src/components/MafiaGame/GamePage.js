a4066a09a57660d6d4f813f20b81175959163ac4
Warren772 <82717393+beyondmafiadev-warrens@users.noreply.github.com>
2021-10-06 04:20:01 -0400

    village and graveyard skeleton of game engine fleshed out
    -need to add notes popup
    -need way to get user image

--- src/components/MafiaGame/GamePage.js
+++ src/components/MafiaGame/GamePage.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
 import PlayerContainer from './PlayerContainer.js'
-
 class GamePage extends Component {
   constructor(props){
       super(props);
 	  this.state = {
+      players : [{name:"test", id:0 }],
+      graveyard :[{name:"test", playerid:0, roleid: 0}],
       }
-  }
+
+}
 render(){
   return(
-<div>
-<PlayerContainer />
-
+<div className="players" style={{float:"left"}}>
+<PlayerContainer players={this.state.players} graveyard={this.state.graveyard}/>
 </div>
 );
 }
--- src/components/MafiaGame/PlayerContainer.js
+++ src/components/MafiaGame/PlayerContainer.js
@@ -1,17 +1,40 @@
 import React, {Component} from 'react';
-
+import './css/PlayerContainer.css'
+import defaultRole from './assets/default-role.png';
 class PlayerContainer extends Component {
   constructor(props){
       super(props);
 	  this.state = {
-      players : []
+      selectedPlayer: -1
       }
-  }
+}
+handlePlayerClick = playerid => () => {
+    this.setState({selectedPlayer:playerid});
+}
+
 render(){
+  var {players, graveyard} = this.props;
+  let playerArr = [];
+  players.map(player => playerArr.push(
+      <div className="player" onClick={this.handlePlayerClick(player.id)}><img src={defaultRole} className="role"/><img className="playerImage" src={"/static/media/kfy8nir1jq131.5c2dc0c7.jpg"} /> <p> {player.name} </p> </div>
+    )
+  )
+  let graveyardElement;
+  if(graveyard.length !== 0){
+    let graveyardArr = [];
+    graveyard.map(player => graveyardArr.push(
+        <div className="player"><img src={defaultRole} className="defaultRole"/><img className="playerImage" src={"/static/media/kfy8nir1jq131.5c2dc0c7.jpg"} /> <p> {player.name} </p> </div>
+      )
+    )
+    graveyardElement = <div className="graveyard"><h1>Graveyard</h1>{graveyardArr}</div>
+  }
   return(
-<div>
-    Village 
-</div>
+    <div className="playerContainer" style={{paddingLeft:'20px'}}>
+    <h1>Village</h1>
+      {playerArr}
+      {graveyardElement}
+    </div>
+
 );
 }
 }
