facad96b217383bc77b07866d86a1644331f7607
beyondmafiadev-warrens <w@greenstar-tech.net>
2022-02-22 10:45:11 -0500

    fixed more formatting of game header

--- src/components/MafiaGame/GamePage.js
+++ src/components/MafiaGame/GamePage.js
@@ -8,8 +8,8 @@ class GamePage extends Component {
       super(props);
 	  this.state = {
 	      players : [{name:"test", id:0 },{name:"test2", id:0},{name:"test3", id:0},{name:"test4", id:0},{name:"test5", id:0},{name:"test6", id:0},{name:"test7", id:0},{name:"test8", id:0}],
-	      graveyard :[{name:"test", playerid:0, roleid: 0}],
-	      messages: [],
+	      graveyard :[{name:"test", playerid:0, roleid: 0}], 
+	      messages: [{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'},{msgType: 0, name: 'test', msg:'hello'}],
       }
 
 }
@@ -22,9 +22,9 @@ render(){
 	    
 	    
 	
-	<div className="gameContainer" style={{display:"flex"}}>	
+	    <div className="gameContainer" style={{display:"flex", paddingTop:"50px"}}>	
 
-	    <div className="players" style={{paddingRight: '30px'}}>
+		<div className="players" style={{paddingRight: "30px"}}>
 	      <PlayerContainer players={this.state.players} graveyard={this.state.graveyard} messages={this.state.messages}/>
 	  </div>
 
