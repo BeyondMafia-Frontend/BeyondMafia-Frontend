b4916f00ebb8b0a306b522c06524c697326a0e5f
beyondmafiadev-warrens <w@greenstar-tech.net>
2022-02-24 12:56:52 -0500

    features added to lobby page

--- src/components/LobbyPage.js
+++ src/components/LobbyPage.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import MafiaHeader from './MafiaHeader.js'
 import Announcements from './Announcements.js'
-
+import Leaderboard from './Leaderboard.js'
+import Competitive from './Competitive.js'
 class LobbyPage extends Component {
   constructor(props){
     super(props);
@@ -14,7 +15,17 @@ render(){
   return(
 	  <div>
 	      <MafiaHeader />
-	      <Announcements />
+	      <div className="info" style={{display:"flex", flexFlow:"column", gap:"15px"}}>
+		  <Announcements />
+		  <div className="divide" style={{paddingTop:'10px'}}>
+		  </div>
+		      
+		  <Leaderboard/ >
+		  <div className="divide" style={{paddingTop:'10px'}}>
+		  </div>
+
+		  <Competitive />
+		  </div>
 	  </div>
   );
 }
