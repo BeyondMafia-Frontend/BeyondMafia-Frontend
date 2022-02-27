ec93bcb5e71e06176371f2a5abc7115a6b3ea2e6
beyondmafiadev-warrens <w@greenstar-tech.net>
2022-02-27 07:58:57 -0500

    game page spinner

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,32 +1,51 @@
 import React, {Component} from 'react';
 import "../css/Games.css"
+import knife from '../assets/knife.png'
 
 class Games extends Component{
     constructor(props){
 	super(props);
 	this.state={
+	    doneLoading: false,
 	}
     }
-
+    
 
     render(){
+	const gameDisplay = () => {
+	switch(this.state.doneLoading){
+	case true:
+	    return;
+	default:
+	    var spinner = <div className="loading">
+		<img src={knife}
+		     className="spinner"
+		     width="100px"
+		     height="100px"
+		     />
+			  </div>
+	    return spinner;
+	}
+	}
+	
 	return(
 	    <div style={{width:"60%", marginLeft:"100px", marginTop:"22px", border: "1px solid", borderRadius:"0px 15px 0px 0px", backgroundColor:'white'}}>
 		<div className="lobbyHeader">
 		    <div className="lobbies">
 			<div className="lobby">
-			    <p> All Lobbies </p> 
+			    <p className="lobbyName"> All Lobbies </p> 
 			</div>
 			<div className="lobby">
-			    <p> Main </p> 
+			    <p className="lobbyName"> Main </p> 
 			</div>
 			<div className="lobby">
-			    <p> Sandbox </p> 
+			    <p className="lobbyName"> Sandbox </p> 
 			</div>
 			<div className="lobby">
-			    <p> Forum Games </p> 
+			    <p className="lobbyName"> Forum Games </p> 
 			</div>
 		    </div>
+		    {gameDisplay()}
 		</div>
 	    </div>
 	)
