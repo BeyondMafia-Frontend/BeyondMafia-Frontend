d5b99298c576abb07ce5cb23867dda9dcaf3b882
Antony Bove <ajbove@buffalo.edu>
2022-07-25 23:30:27 -0400

    player page css outline

--- src/components/PlayerPage.js
+++ src/components/PlayerPage.js
@@ -13,6 +13,24 @@ class PlayerPage extends Component {
   return(
     
     <div class='container'>
+        <div class='header'>Header</div>
+        <div class='banner'>Banner</div>
+        <div class='banner'>Banner</div>
+        <div class='msg-bar'>msg and settings</div>
+        <div class='bar-left'>Shoutbox</div>
+        <div class='bar-right'>Collections</div>
+        <div class='bio-box'>bio</div>
+        <div class='shout-chat'>shout chat..</div>
+        <div class='collect-content'>collections content</div>
+        <div class='bar-left'>friends</div>
+        <div class='bar-right'>Favs</div>
+        <div class='friends-content'>my friends</div>
+        <div class='favs-content'> my Favs</div>
+        <div class='stat-bar'> USER STATS</div>
+        <div class='stat-content'> USER STATS</div>
+
+
+    
 
     </div>
   );
