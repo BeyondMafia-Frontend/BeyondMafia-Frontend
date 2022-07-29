67f9954286a756f7a431ade4ee996f8609d28499
Antony Bove <ajbove@buffalo.edu>
2022-07-29 12:45:56 -0400

    front end changes

--- src/components/PlayerPage.js
+++ src/components/PlayerPage.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import logo from '../assets/logo-light.png';
 import '../css/PlayerPage.css'
+import playerpic from '../assets/default-avis/neuron dark.png';
+import msg from '../assets/msg.png'
 
 class PlayerPage extends Component {
   constructor(props){
@@ -13,10 +15,35 @@ class PlayerPage extends Component {
   return(
     
     <div class='container'>
-        <div class='header'>Header</div>
-        <div class='banner'>Banner</div>
-        <div class='banner'>Banner</div>
-        <div class='msg-bar'>msg and settings</div>
+        <div class='header'>
+        <img 
+			    src={logo}
+			    id="logoLightplayer"
+			    alt="BeyondMafia"
+          
+		    />
+        <a class='nav-links'href=''>L O B B Y</a>
+        <p class='nav-divider'>|</p>
+        <a class='nav-links'href=''>P L A Y E R</a>
+        <p class='nav-divider'>|</p>
+        <a class='nav-links' href=''>R O U N D</a>
+        <p class='nav-divider'>|</p>
+        <a class='nav-links' href=''>F O R U M</a>
+        <p class='nav-divider'>|</p>
+        <a class='nav-links' href=''>L E A R N</a>
+        <img src={playerpic} id="player-pfp" alt="BeyondMafia"/>
+        </div>
+        <div class='banner'></div>
+        <div class='user-banner'>
+            <p class='user-name'>username</p>
+            <div class='user-banner-bot'>
+            </div>
+        </div>
+        <div class='msg-bar'>
+          <img src={msg} id='msg' alt="msg"/>
+          <p>Message</p>
+        </div>
+        
         <div class='bar-left'>Shoutbox</div>
         <div class='bar-right'>Collections</div>
         <div class='bio-box'>bio</div>
@@ -28,6 +55,7 @@ class PlayerPage extends Component {
         <div class='favs-content'> my Favs</div>
         <div class='stat-bar'> USER STATS</div>
         <div class='stat-content'> USER STATS</div>
+        <img src={playerpic} id='profile' alt='banner profile'/> 
 
 
     
