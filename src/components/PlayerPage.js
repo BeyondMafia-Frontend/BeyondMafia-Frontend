3ad0ade6bd6cc303590285e1f3bd7eb0719bb162
beyondmafiadev-warrens <82717393+beyondmafiadev-warrens@users.noreply.github.com>
2022-09-12 04:46:47 -0400

    player page stylistic updates

--- src/components/PlayerPage.js
+++ src/components/PlayerPage.js
@@ -46,37 +46,58 @@ class PlayerPage extends Component {
   }
   render(){
   return(
-
+    <div>
+    <div class='header'>
+    <a href="/">
+    <img
+      src="/assets/logo-light.png"
+      id="logoLightplayer"
+      alt="BeyondMafia"
+    />
+    </a>
+    <a class='nav-links'href='/lobby'>L O B B Y</a>
+    <p class='nav-divider'>|</p>
+    {parseInt(this.state.playerid) === parseInt(this.state.currentId)
+    ?
+    <a class='nav-links-highlighted'>P L A Y E R</a>
+    : this.state.playerid === -1
+      ?<a class='nav-links'>P L A Y E R</a>
+      :<a class='nav-links' href={'/players/'+this.state.playerid.toString()}>P L A Y E R</a>
+    }
+    <p class='nav-divider'>|</p>
+    <a class='nav-links'>R O U N D</a>
+    <p class='nav-divider'>|</p>
+    <a class='nav-links' >F O R U M</a>
+    <p class='nav-divider'>|</p>
+    <a class='nav-links'>L E A R N</a>
+    <img src="/assets/default-avis/neurondark.png" id="player-pfp" alt="BeyondMafia"/>
+    </div>
     <div class='container'>
-        <div class='header'>
-        <a href="/">
-        <img
-          src="/assets/logo-light.png"
-          id="logoLightplayer"
-          alt="BeyondMafia"
-        />
-        </a>
-        <a class='nav-links'href='/lobby'>L O B B Y</a>
-        <p class='nav-divider'>|</p>
-        {parseInt(this.state.playerid) === parseInt(this.state.currentId)
-        ?
-        <a class='nav-links-highlighted'>P L A Y E R</a>
-        : this.state.playerid === -1
-          ?<a class='nav-links'>P L A Y E R</a>
-          :<a class='nav-links' href={'/players/'+this.state.playerid.toString()}>P L A Y E R</a>
-        }
-        <p class='nav-divider'>|</p>
-        <a class='nav-links'>R O U N D</a>
-        <p class='nav-divider'>|</p>
-        <a class='nav-links' >F O R U M</a>
-        <p class='nav-divider'>|</p>
-        <a class='nav-links'>L E A R N</a>
-        <img src="/assets/default-avis/neurondark.png" id="player-pfp" alt="BeyondMafia"/>
-        </div>
-        <div class='banner'></div>
+
+        <div class='banner'>
+        <img src="/assets/default-avis/neurondark.png" id='profile' alt='banner profile'/>
+</div>
         <div class='user-banner'>
             <p class='user-name'>{this.state.username}</p>
             <div class='user-banner-bot'>
+            </div>
+            <div class="leftStats">
+            <img src="/assets/points.png"
+            width="50px"
+            height="50px"
+            alt="Points"
+            title="Points"
+            />
+            <div class="stats"> 0
+            </div>
+            <img src="/assets/gem.png"
+            width="50px"
+            height="50px"
+            alt="Gems"
+            title="Gems"
+            style={{marginTop:"-7px",paddingLeft:"5px"}}
+            />   <div class="stats"> 0
+              </div>
             </div>
         </div>
         <div class='msg-bar'>
@@ -84,24 +105,26 @@ class PlayerPage extends Component {
           <p>Message</p>
         </div>
 
+        <div class="secondLayer">
+        <div class="left-profile">
         <div class='bar-left'>Shoutbox</div>
-        <div class='bar-right'>Collections</div>
+        <div class='shout-chat'>{this.state.username} has no comments in their shoutbox!</div>
+        <div class='bar-left'>Friends</div>
+        <div class='friends-content'>{this.state.username} has no friends!</div>
+        </div>
         <div class='bio-box'>{this.state.bio === null
                               ? "N/A"
                               : this.state.bio}</div>
-        <div class='shout-chat'>Coming soon...</div>
-        <div class='collect-content'>Coming soon...</div>
-        <div class='bar-left'>friends</div>
-        <div class='bar-right'>Favs</div>
-        <div class='friends-content'>Coming soon...</div>
-        <div class='favs-content'>Coming soon...</div>
+        <div class="right-profile">
+        <div class='bar-right'>Collections</div>
+        <div class='collect-content'>{this.state.username} has no items in their collection!</div>
+        <div class='bar-right'>Favorites</div>
+        <div class='favs-content'>{this.state.username} has no favorites!</div>
+        </div>
+        </div>
         <div class='stat-bar'> {this.state.username}'s Stats </div>
-        <div class='stat-content'> USER STATS</div>
-        <img src="/assets/default-avis/neurondark.png" id='profile' alt='banner profile'/>
-
-
-
-
+        <div class='stat-content'> {this.state.username} has no stats!</div>
+    </div>
     </div>
   );
   }
