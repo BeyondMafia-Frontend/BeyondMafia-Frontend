12c9352f0eb994e5abeea641b820344cace8b786
beyondmafiadev-warrens <82717393+beyondmafiadev-warrens@users.noreply.github.com>
2022-09-15 01:49:55 -0400

    Update ChatContainer.js

--- src/components/MafiaGame/ChatContainer.js
+++ src/components/MafiaGame/ChatContainer.js
@@ -67,9 +67,9 @@ handlePlayerClick = playerName => () => {
 
 async parseSettingsMessage(command){
   if(this.state.parsed === false){
+  this.setState({parsed:true});
   this.setState({currentGameState:command.state})
   await this.props.setGameSettings(command);
-  this.setState({parsed:true});
 }
 }
 
@@ -167,7 +167,7 @@ render(){
   var {messages} = this.props;
   var messagesArr = [];
 
-  messages.map(message => {
+  messages.map(async(message) => {
     let messageElement;
     var command = JSON.parse(message);
     if(command.cmd === -4){
@@ -210,7 +210,6 @@ render(){
       messageElement = this.parseLeaveMessage(command);
     }
     if(command.cmd === 7){
-      if(this.state.parsed){
       this.setState({currentGameState:command.state});
       this.props.updateGameState(command.state);
       this.iterateMessages(command.state);
@@ -218,19 +217,16 @@ render(){
       this.setState({messagesQueue:[]})
       this.props.setMessageBankLength(Object.keys(this.state.messageBank).length);
       messages.shift();
-    }
       return;
     }
     if(command.cmd === 8){
       this.props.addPlayer({name: command.playerid, playerid:  command.playerid})
     }
     if(command.cmd === 9){
-      this.parseSettingsMessage(command);
+      await this.parseSettingsMessage(command);
     }
-    if(this.state.parsed){
     messages.shift();
     this.setState({messages: [...this.state.messages, messageElement]})
-  }
   });
 var chatContainer = document.getElementsByClassName('chatContainer')[0];
 if(chatContainer){
